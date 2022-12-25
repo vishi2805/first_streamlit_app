bb5a30fd8c0200eb95e871e743ab88345ae7bc19
Vishal Kumar <121333626+vishi2805@users.noreply.github.com>
2022-12-25 14:04:23 +0530

    Update streamlit_app.py
    added Imoji

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega3 & Blueberry Oatmeal')
-streamlit.text('Kela, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('U+1F964 Omega3 & Blueberry Oatmeal')
+streamlit.text('U+1F957 Kela, Spinach & Rocket Smoothie')
+streamlit.text('U+1F414	 Hard-Boiled Free-Range Egg')
+streamlit.text('U+1F951 U+1F35E Avcado Toast')
