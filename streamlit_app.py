501fb769e4ad839c4ff34096c35d53b8b9f1c906
Vishal Kumar <121333626+vishi2805@users.noreply.github.com>
2022-12-25 14:30:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,5 @@ streamlit.text('\U0001F963 Omega3 & Blueberry Oatmeal')
 streamlit.text('\U0001F957 Kela, Spinach & Rocket Smoothie')
 streamlit.text('\U0001F414 Hard-Boiled Free-Range Egg')
 streamlit.text('\U0001F951 \U0001F35E Avcado Toast')
+
+streamlit.header('\U0001F34C \U0001F96D	Build Your Own Fruit Smoothie \U0001F95D \U0001F347')
