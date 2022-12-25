d3ee3ae1c6a340718392e38a307ea2cc8af7125a
Vishal Kumar <121333626+vishi2805@users.noreply.github.com>
2022-12-25 14:21:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,9 @@
 import streamlit
-pip install imoji
 
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('\U0001F964 'Omega3 & Blueberry Oatmeal')
+streamlit.text('\U0001F964' 'Omega3 & Blueberry Oatmeal')
 streamlit.text('\U0001F957 Kela, Spinach & Rocket Smoothie')
 streamlit.text('\U0001F414	 Hard-Boiled Free-Range Egg')
 streamlit.text('\U0001F951 U+1F35E Avcado Toast')
