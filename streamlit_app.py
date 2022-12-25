99faecd49716bccd4e58469af840e056bba8ebe0
Vishal Kumar <121333626+vishi2805@users.noreply.github.com>
2022-12-25 14:36:49 +0530

    Update streamlit_app.py
    added Pandas library and reading cloud csv file

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,7 @@ streamlit.text('\U0001F414 Hard-Boiled Free-Range Egg')
 streamlit.text('\U0001F951 \U0001F35E Avcado Toast')
 
 streamlit.header('\U0001F34C \U0001F96D	Build Your Own Fruit Smoothie \U0001F95D \U0001F347')
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
